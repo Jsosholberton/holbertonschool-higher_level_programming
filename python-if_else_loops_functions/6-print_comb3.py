499aa01dc086b181b27de22b7311199944a57be3
root <root@Johnatan>
2023-06-01 13:47:30 -0500

    print combination

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    print("")
+for first_digit in range(10):
+    for second_digit in range(first_digit+1, 10):
+        if (first_digit != 8 or second_digit != 9):
+            print("{}{}".format (first_digit, second_digit), end = ", ")
+        else:
+            print("{}{}".format (first_digit, second_digit))

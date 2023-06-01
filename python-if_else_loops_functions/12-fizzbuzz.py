5765ee983250fbb622082ac8be978fa958f5c885
root <root@Johnatan>
2023-06-01 14:43:24 -0500

    FizzBuzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for index in range(1, 100):
+        if (index % 3) == 0 and (index % 5) == 0:
+            print(f"FizzBuzz", end = " ")
+        elif (index % 3) == 0:
+            print(f"Fizz", end=" ")
+        elif (index % 5) == 0:
+            print(f"Buzz", end= " ")
+        else:
+            print(f"{index}", end = " ")

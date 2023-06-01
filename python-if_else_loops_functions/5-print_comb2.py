b8d20ca9ac82ead807553ebbd68e541d45cc37c6
root <root@Johnatan>
2023-06-01 09:59:33 -0500

    try

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     if number != 99:
-        print(f'{number:02d}', end= ", ")
-print(f"{number}")
+        print(f'{number:02d}, ', end="")
+print(f"{number}", end='\n')

c0a2e91cd56fe12b5259778a261c7ebfd21850c4
root <root@Johnatan>
2023-06-01 14:48:04 -0500

    pystyle

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for index in range(1, 101):
         if (index % 3) == 0 and (index % 5) == 0:
-            print(f"FizzBuzz", end = " ")
+            print(f"FizzBuzz", end=" ")
         elif (index % 3) == 0:
             print(f"Fizz", end=" ")
         elif (index % 5) == 0:
-            print(f"Buzz", end= " ")
+            print(f"Buzz", end=" ")
         else:
-            print(f"{index}", end = " ")
+            print(f"{index}", end=" ")

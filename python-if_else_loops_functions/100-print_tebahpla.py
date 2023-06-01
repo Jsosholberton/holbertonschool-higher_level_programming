710aa0fadbea348a652de9dbb23592ee7d8789be
root <root@Johnatan>
2023-06-01 15:10:00 -0500

    solved?

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for aBcD in range(122, 96, -2):
-    tmp = chr(aBcD)
-    upper = chr(aBcD - 1 - 32)
-    print(f'{tmp}{upper}', end = "")
+    upper = aBcD - 1 - 32
+    print("{}{}".format(chr(aBcD), chr(upper)), end="")

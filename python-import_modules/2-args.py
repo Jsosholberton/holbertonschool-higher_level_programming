e3899ad5c25b97163fbc527c2f8ecda96ad82714
root <root@Johnatan>
2023-06-02 11:25:00 -0500

    try solve

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
-size = len(argv) - 1
-if size == 0:
-    print("{} arguments.".format(size))
-else:
-    if size == 1:
-        print("{} argument:".format(size))
+if __name__ == "__main__":
+    size = len(argv) - 1
+    if size == 0:
+        print("{} arguments.".format(size))
     else:
-        print("{} arguments:".format(size))
-    for i in range(1, size + 1):
-        print("{}: {}".format(i, argv[i]))
+        if size == 1:
+            print("{} argument:".format(size))
+        else:
+            print("{} arguments:".format(size))
+        for i in range(1, size + 1):
+            print("{}: {}".format(i, argv[i]))

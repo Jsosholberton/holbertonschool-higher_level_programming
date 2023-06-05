53feffd1e4915a2d87cd0dbff03e2d895c87aa1e
root <root@Johnatan>
2023-06-05 11:00:53 -0500

    solved?

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,9 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for i in range(0, len(matrix)):
             for j in range(0, len(matrix[i])):
-                print('{}'.format(matrix[i][j]), end=' ')
+                if j == len(matrix):
+                    ended = ''
+                else:
+                    ended = ' '
+                print('{}'.format(matrix[i][j]), end=ended)
             print()

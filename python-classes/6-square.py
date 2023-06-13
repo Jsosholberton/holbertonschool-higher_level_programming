ff33445b097245c9ef9a40842dbcef915ded433e
root <root@Johnatan>
2023-06-13 09:50:28 -0500

    fixed?

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -45,7 +45,7 @@ class Square():
         """print the square using '#'"""
 
         if self.size == 0:
-                    print()
+            print()
         else:
             for height in range(self.position[1]):
                 print()
@@ -68,5 +68,6 @@ class Square():
         integer and >= 0"""
 
         self.__position = value
-        if type(value) is not tuple or value[0] < 0 or value[1] < 0:
+        if (type(value) is not tuple or value[0] < 0 or value[1] < 0
+           or len(value) != 2):
             raise ValueError("position must be a tuple of 2 positive integers")

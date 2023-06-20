9327b368355e699abe6a824130c8152dac1da4e4
root <root@Johnatan>
2023-06-20 08:40:46 -0500

    Without documentation

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
-"""Function that return a copy of a list"""
-
-
 def copy_list(a_list):
-    """
-    Args:
-    -----
-        a_list (list): list to clone or copy
-
-    Return:
-    -------
-        Newest list
-    """
-
     return a_list[:]

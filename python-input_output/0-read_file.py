a450297b959ae3d94a778c806f9959191a52a058
root <root@Johnatan>
2023-06-23 13:17:46 -0500

    first commit

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Definition of the function for read a file"""
+
+
+def read_file(filename=""):
+    """
+    Description:
+    ------------
+        Read a file
+    Args:
+    -----
+        filename (str): name of the file to be read
+    """
+
+    with open(filename, encoding='utf-8') as file_read:
+        print(file_read.read())

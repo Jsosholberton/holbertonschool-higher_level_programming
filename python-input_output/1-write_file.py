18c0403a8aa27c4d5b5363837b850055bad35e66
root <root@Johnatan>
2023-06-23 13:29:07 -0500

    write

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Definition of the function"""
+
+
+def write_file(filename="", text=""):
+    """
+    Description:
+    ------------
+        Write into a file
+    Args:
+    ----
+        filename (str): name of the file
+        text (str): the text into the file
+    Return:
+        The file with the text
+    """
+
+    with open(filename, mode='w', encoding='utf-8') as file:
+        return (file.write(text))

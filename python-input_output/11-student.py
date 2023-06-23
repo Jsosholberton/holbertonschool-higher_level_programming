a4fa8c392f5dceecd353570a925e2a1e3a80ff30
root <root@Johnatan>
2023-06-23 15:30:36 -0500

    student 3.0

---
+++ python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Class Student"""
+
+
+class Student:
+    """Definition of the class"""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Args:
+        -----
+            first_name (str): value first name
+            last_name (str): value last name
+            age (int): value of the age
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return dictionary with simple data structure"""
+        if isinstance(attrs, list) and ([isinstance(n, str)] for n in attrs):
+            new_dict = {}
+            for words in attrs:
+                if words in self.__dict__:
+                    new_dict[words] = self.__dict__[words]
+            return new_dict
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """that replaces all attributes"""
+        for names in json:
+            self.__dict__[names] = json[names]

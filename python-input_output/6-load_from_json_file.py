830ca4fcd9ea1c1110af4cfca7f37769ae17d8bd
root <root@Johnatan>
2023-06-23 14:24:32 -0500

    load from json file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""""""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Description:
+    -----------
+        Crates an objext by a json file
+    Args:
+    -----
+        filename (str): name of the file to be readed
+    Return:
+    -------
+        an object by the json file
+    """
+    with open(filename, encoding='utf-8') as file:
+        return json.load(file)

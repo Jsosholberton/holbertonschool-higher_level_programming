9c70385c4f8c8339e2dbe0d3ff90ec67856850e3
root <root@Johnatan>
2023-06-26 09:12:50 -0500

    first commit

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Definition of the class 'Base'"""
+
+
+class Base:
+    """
+    Description:
+    ------------
+        This is the base of the project
+    Args:
+    -----
+        nb_objects (int): count num of objects, and set in id
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Description:
+        -----------
+            Definition of the class init
+        Args:
+        ----
+            id (int): the id of the process?
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
